766351490572efdb0f9ce9b2ca999210e992375c
Sachin Kumar Singh <sachin.kumar.singh@accenture.com>
2016-10-21 16:10:40 +0000

    Upgrade sonarqube to 5.6.2 version

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM sonarqube:5.1.2
+FROM sonarqube:5.6.2
 
 MAINTAINER Robert Northard, <robert.a.northard>
 
--- resources/plugins.sh
+++ resources/plugins.sh
@@ -8,6 +8,19 @@ set -e
 mkdir -p ${SONARQUBE_PLUGINS_DIR}
 
 while read plugin; do
-    echo "Downloading ${plugin##*/}"
-    curl -s -L -f ${plugin} -o ${SONARQUBE_PLUGINS_DIR}/${plugin##*/}
+    fullname=$(basename "$plugin")
+    extension="${fullname##*.}"
+    filename="${fullname%.*}"
+    echo "Downloading ${fullname}"
+    case ${extension} in 
+        'jar') curl -s -L -f ${plugin} -o ${SONARQUBE_PLUGINS_DIR}/${fullname}
+		       ;;
+        'zip') curl -s -L -f ${plugin} -o /tmp/${fullname}
+               cd ${SONARQUBE_PLUGINS_DIR}
+               unzip -j /tmp/${fullname} "*${filename}.jar"
+               rm -rf /tmp/${fullname}
+               ;;
+        *) "ERROR: Unable to download ${fullname} doe to unsupported download url."
+               ;;
+    esac
 done  < $1
